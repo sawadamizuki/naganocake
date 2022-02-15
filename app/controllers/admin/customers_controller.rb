cbd1afba3b0fbc86edc8e0799c1c4f138c8ad424
sawadamizuki <m.sawada3249@gmail.com>
2022-02-15 15:49:42 +0000

    [add]会員詳細・編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,12 +5,25 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customers = Customer.find(params[:id])
   end
 
   def edit
+    @customers = Customer.find(params[:id])
   end
 
   def update
+    customers = Customer.find(params[:id])
+    if customers.updated(customers_params)
+      redirect_to admin_customer_path(customers)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def customers_params
+    params.require(:customers).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_active)
   end
 
 end
