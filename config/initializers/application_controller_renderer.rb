0dc8f5a020f827328940c12cfc440f991af4f040
sawadamizuki <m.sawada3249@gmail.com>
2022-02-23 16:00:40 +0000

    [add]商品詳細・一覧ページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'aabd35e7a9d397a61d522e956de38cb7f9198192fd7e4d2cda2f7a51c752268347e1d2507799305b049b5dda32f845d2e1b7a620474f431ca227197fa4d67739'
