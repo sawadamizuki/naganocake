1f55cae0bf515236936efc07cb924f9a2eacf7b2
sawadamizuki <m.sawada3249@gmail.com>
2022-03-05 12:44:27 +0000

    [update]注文情報入力画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,7 @@
 class Public::AddressesController < ApplicationController
 
   def index
-    @addresses = Address.all
+    @addresses = current_customer.addresses
     @address = Address.new
   end
 
